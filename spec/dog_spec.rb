9f8e98a89e65962406b4b1a80efe14e0d46bc959
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-01 17:10:04 -0500

    remove test suite dependency on clear_all method, refactored tests to use let blocks

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -1,42 +1,40 @@
 require "spec_helper"
 
 describe "Dog" do 
+
+  let!(:pluto) { Dog.new("Pluto") }
+  let!(:fido) { Dog.new("Fido") }
+  let!(:maddy) { Dog.new("Maddy") }
   
   after(:each) do 
-    Dog.clear_all
+    Dog.class_variable_set(:@@all, [])
   end
   
   describe "Dog class variables" do 
     it "has a class variable, @@all, the points to an array" do 
-      expect(Dog.class_variable_get(:@@all)).to eq([] )
+      expect(Dog.class_variable_get(:@@all)).to be_a(Array)
     end
   end
 
-  describe ".clear_all" do 
+  describe "#clear_all" do 
     it "is a class method that empties the @@all array of all existing dogs" do 
-      Dog.new("Pluto")
-      Dog.new("Maddy")
       Dog.clear_all
       expect(Dog.class_variable_get(:@@all)).to match([]  )
     end
   end
 
-  describe ".all" do
+  describe "#all" do
     it "is a class method that puts out the name of each dog to the terminal" do 
-      Dog.new("Pluto")
-      Dog.new("Maddy")
-      expect{Dog.all}.to output("Pluto\nMaddy\n").to_stdout
+      expect{Dog.all}.to output("Pluto\nFido\nMaddy\n").to_stdout
     end 
   
   describe "#new" do 
     it "initializes with an argument of a name" do 
-      expect{Dog.new("Pluto")}.to_not raise_error
+      expect{Dog.new("Snoopy")}.to_not raise_error
     end
 
     it "adds the new dog to the @@all array" do
-      pluto = Dog.new("Pluto")
-      fido = Dog.new("Fido")
-      expect(Dog.class_variable_get(:@@all)).to match([pluto, fido]) 
+      expect(Dog.class_variable_get(:@@all)).to match([pluto, fido, maddy]) 
     end
   end
 
@@ -44,7 +42,6 @@ describe "Dog" do
     
   describe "#name" do 
     it "has a name" do 
-      pluto = Dog.new("Pluto")
       expect(pluto.name).to eq("Pluto")
     end
   end
