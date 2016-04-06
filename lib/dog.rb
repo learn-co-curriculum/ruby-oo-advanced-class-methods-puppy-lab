18d2f7153c18eb444c9c967a21a8d1ce19aad840
Charles Pletcher <cpletcher2@gmail.com>
2016-04-06 13:45:01 -0400

    Add a solution
    Fixes #3

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,8 +9,7 @@ class Dog
   end
 
   def self.all
-    # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
-    puts @@all.map{ |dog| dog.name }
+    puts @@all.map(&:name)
   end
 
   def self.clear_all
