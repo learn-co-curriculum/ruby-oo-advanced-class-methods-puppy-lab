5b4b4d85ea156a9f483894c794cf01200e307944
Charles Pletcher <cpletcher2@gmail.com>
2016-04-06 13:45:01 -0400

    Add a solution
    Fixes #3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    puts @@all.map(&:name)
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
