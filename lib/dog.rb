582437e25853b9cde19f784e6076b20dff32f65d
maxwellbenton <maxwellbenton@gmail.com>
2019-06-18 13:36:56 -0400

    updates solution

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,10 @@ class Dog
   end
 
   def self.all
+    @@all
+  end
+
+  def self.print_all
     # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
     puts @@all.map{ |dog| dog.name }
   end
