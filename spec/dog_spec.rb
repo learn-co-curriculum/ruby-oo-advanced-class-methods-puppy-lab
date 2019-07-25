45ed89fe24a9942ef41d974487f7c6daf88d61c8
maxwellbenton <maxwellbenton@gmail.com>
2019-07-25 12:04:32 -0400

    updates spec to include save

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -1,7 +1,12 @@
 require "spec_helper"
 
-
 describe "Dog" do
+
+  describe ".new" do
+    it "initializes with one argument - a name" do
+      expect{Dog.new("Snoopy")}.to_not raise_error
+    end
+  end
 
   let!(:pluto) { Dog.new("Pluto") }
   let!(:fido) { Dog.new("Fido") }
@@ -11,37 +16,48 @@ describe "Dog" do
     Dog.class_variable_set(:@@all, [])
   end
 
-  describe "Dog class variables" do
-    it "has a class variable, @@all, the points to an array" do
-      expect(Dog.class_variable_get(:@@all)).to be_a(Array)
+  describe "class variables" do
+    describe "@@all" do
+      it "has a class variable, @@all, the points to an array" do
+        expect(Dog.class_variable_get(:@@all)).to be_a(Array)
+      end
     end
   end
 
-  describe ".all" do
-    it "is a class method returns all dog instances" do
-      expect(Dog.all).to match([pluto,fido,maddy])
+  describe "class methods" do
+    describe ".all" do
+      it "is a class method returns all dog instances" do
+        expect(Dog.all).to match([pluto,fido,maddy])
+      end
+    end
+  
+    describe ".clear_all" do
+      it "is a class method that empties the @@all array of all existing dogs" do
+        Dog.clear_all
+        expect(Dog.class_variable_get(:@@all)).to match([]  )
+      end
+    end
+  
+    describe ".print_all" do
+      it "is a class method that puts out the name of each dog to the terminal" do
+        expect{Dog.print_all}.to output("Pluto\nFido\nMaddy\n").to_stdout
+      end
     end
   end
+  
 
-  describe ".clear_all" do
-    it "is a class method that empties the @@all array of all existing dogs" do
+  describe "#save" do
+    it "adds this dog instance to the @@all array when called" do
       Dog.clear_all
-      expect(Dog.class_variable_get(:@@all)).to match([]  )
+      pluto.save
+      expect(Dog.class_variable_get(:@@all)).to match([pluto])
+      maddy.save
+      expect(Dog.class_variable_get(:@@all)).to match([pluto, maddy])
     end
-  end
 
-  describe ".print_all" do
-    it "is a class method that puts out the name of each dog to the terminal" do
-      expect{Dog.print_all}.to output("Pluto\nFido\nMaddy\n").to_stdout
-    end
-  end
-
-  describe ".new" do
-    it "initializes with an argument of a name" do
-      expect{Dog.new("Snoopy")}.to_not raise_error
-    end
-    it "adds the new dog to the @@all array" do
-      expect(Dog.class_variable_get(:@@all)).to match([pluto, fido, maddy])
+    it "gets called inside initialize when a new Dog is created" do
+        expect_any_instance_of(Dog).to receive(:save)
+        Dog.new('Luca')
     end
   end
 
