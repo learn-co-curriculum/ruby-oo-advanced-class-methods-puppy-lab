d3822e8b25989a5b3386786c0ceae1d72bc8627e
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-11-07 17:01:09 -0600

    commit tree 87ca959a08891aed3efe9f226d0f7955d79b1eaa

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+
+class Dog
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name=name
+        # @@all << self
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.collect {|songs| puts songs.name}
+    end
+
+    def save 
+        @@all << self
+    end
+end
