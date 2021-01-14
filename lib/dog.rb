62dc8975b38fef4ef1ee8637c3cc91f8b00cad50
David Polcari <dpolcari18@gmail.com>
2021-01-13 19:13:10 -0600

    all tests passing

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    @@all = []
+    
+    attr_reader :name
+
+    def initialize (name)
+        @name = name
+        save()
+    end
+
+    def self.all
+       @@all 
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def save
+        @@all.push(self)
+    end
+end
