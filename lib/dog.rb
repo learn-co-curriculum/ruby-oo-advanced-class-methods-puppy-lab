4dbcf55402d52a5dd693a9560ccbe57433f498fe
jamescross813 <74753385+jamescross813@users.noreply.github.com>
2021-02-06 11:41:43 -0600

    Update dog.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog
+    @@all = []
+
+    
+def initialize(name)
+    @name = name
+    self.save
+end
+
+def self.all
+    @@all
+end
+
+def self.print_all
+    @@all.each do |dog|
+        puts dog.name
+    end    
+end
+
+def self.clear_all
+    @@all.clear
+end
+
+def name 
+    @name
+end
+
+def save
+    @@all << self
+end
