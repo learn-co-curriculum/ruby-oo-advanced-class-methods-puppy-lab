3000df9259dcadeed80a3b5e460813fb45f67116
jamescross <crossjames813@gmail.com>
2021-02-07 14:41:39 -0600

    commit tree 44fb4c5be0f2c122b1ed55435bde44d31dd6ba39

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+# Add your code here
+class Dog
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        self.save
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def self.clear_all
+        self.all.clear
+    end
+    
+    def self.print_all
+    self.all.each {|dog| puts dog.name}
+    end
+    
+    def name
+    @name 
+    end
+    
+    def save
+        @@all << self
+    end
+    end
+    
